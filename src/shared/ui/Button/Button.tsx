f626576656d31a3a16b27c307bd922075677543a
Никита Шамара <nikitashamara@gmail.com>
2023-06-28 12:01:38 +0600

    fix tests

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -23,8 +23,9 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-    const { className, children, theme, square, size, ...otherProps } = props;
-
+    const {
+        className, children, theme, square, size, ...otherProps
+    } = props;
     const mods: Record<string, boolean> = {
         [cls[theme]]: true,
         [cls.square]: square,
